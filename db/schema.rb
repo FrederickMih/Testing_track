941734a7edffc53f68edacdd3a6889ac452083b7
FrederickMih <fredtimermih@gmail.com>
2021-09-24 14:50:21 +0100

    Migrate measures table

--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,3 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2021_09_24_131431) do
-
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "measurements", force: :cascade do |t|
@@ -24,4 +9,14 @@ ActiveRecord::Schema.define(version: 2021_09_24_131431) do
     t.index ["photo"], name: "index_measurements_on_photo"
   end
 
+  create_table "measures", force: :cascade do |t|
+    t.float "data"
+    t.bigint "measurement_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["data"], name: "index_measures_on_data"
+    t.index ["measurement_id"], name: "index_measures_on_measurement_id"
+  end
+
+  add_foreign_key "measures", "measurements"
 end
