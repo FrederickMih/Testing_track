981930dbff3fbd65e7c24048c5de0221ca0c66f2
FrederickMih <fredtimermih@gmail.com>
2021-09-26 03:35:11 +0100

    Add a test for the Measure model

--- app/models/measurement.rb
+++ app/models/measurement.rb
@@ -3,5 +3,6 @@ class Measurement < ApplicationRecord
   has_many :measures, dependent: :destroy
 
    # validations
-  validates_presence_of :name, :photo
+  validates_presence_of :name, presence: true, length: { minimum: 4, maximum: 30 }
+  validates_presence_of :photo
 end
