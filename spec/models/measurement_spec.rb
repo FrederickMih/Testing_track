b8df2629ed161c7aa347a9fea59ec4eeaa4de0b7
FrederickMih <fredtimermih@gmail.com>
2021-09-26 03:12:56 +0100

    Add 1:m Measurement relationship test

--- spec/models/measurement_spec.rb
+++ spec/models/measurement_spec.rb
@@ -2,6 +2,8 @@
 require 'rails_helper'
 
 RSpec.describe Measurement, type: :model do
+   # ensure Meaurement model has a 1:m relationship with the measure model
+  it { should have_many(:measures).dependent(:destroy) }
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:photo) }
   end
