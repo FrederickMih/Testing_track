c4143f0b32bcd632aae2389b875e1c4a0d6c6a77
FrederickMih <fredtimermih@gmail.com>
2021-09-27 20:36:15 +0100

    Modify the measure and measurement controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :measures
-  resources :measurements
+  namespace :api do
+    namespace :v1 do  
+      resources :measures
+      resources :measurements
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
